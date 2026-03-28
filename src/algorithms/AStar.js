aaaff6a4da841bad638f1dfb3e3a15427e13fdbf
Kim Ngân <nhkngan2430@clc.fitus.edu.vn>
2026-03-28 22:09:09 +0700

    feat: implement MazeGenerator component and add A* and Beam Search algorithms

--- src/algorithms/AStar.js
+++ src/algorithms/AStar.js
@@ -8,8 +8,6 @@ class Cell {
   }
 }
 
-// ================= COMMON =================
-
 // kiểm tra hợp lệ
 function isValid(grid, row, col) {
   return row >= 0 && row < grid.length && col >= 0 && col < grid[0].length;
@@ -29,15 +27,6 @@ function isDestination(row, col, dest) {
 function calculateH(row, col, dest) {
   return Math.hypot(row - dest[0], col - dest[1]);
 }
-
-// in path
-function printPath(path) {
-  console.log(path.map(p => `[${p[0]}, ${p[1]}]`).join(" -> "));
-}
-
-function printCost(cost) {}
-
-// ================= A* =================
 
 function tracePath(cellDetails, dest, exploredCount, exploredNodes) {
   let path = [];
@@ -124,90 +113,4 @@ function aStarSearch(grid, src, dest) {
   return { path: [], pathCost: 0, exploredCount: exploredCount, exploredNodes: exploredNodes };
 }
 
-// ================= BEAM SEARCH =================
-
-function beamSearch(grid, src, dest, k = 2) {
-  let closed = [];
-  for (let i = 0; i < grid.length; i++) {
-    closed[i] = [];
-    for (let j = 0; j < grid[0].length; j++) {
-      closed[i][j] = false;
-    }
-  }
-
-  let cell = [];
-  for (let i = 0; i < grid.length; i++) {
-    cell[i] = [];
-    for (let j = 0; j < grid[0].length; j++) {
-      cell[i][j] = new Cell();
-    }
-  }
-
-  let [i, j] = src;
-
-  cell[i][j] = {
-    f: 0,
-    g: 0,
-    h: 0,
-    parent_i: i,
-    parent_j: j
-  };
-
-  let open = [{ f: 0, i, j }];
-  let exploredCount = 0;
-  let exploredNodes = [];
-
-  let dirs = [
-    [0, 1],
-    [0, -1],
-    [1, 0],
-    [-1, 0]
-  ];
-
-  while (open.length) {
-    let newOpen = [];
-
-    for (let node of open) {
-      let { i, j } = node;
-
-      closed[i][j] = true;
-      exploredCount++;
-      exploredNodes.push([i, j]);
-
-      for (let [di, dj] of dirs) {
-        let ni = i + di,
-          nj = j + dj;
-
-        if (!isValid(grid, ni, nj) || !isValidCell(grid, ni, nj) || closed[ni][nj]) continue;
-
-        let cost = grid[ni][nj] === 3 ? 3 : 1;
-        let gNew = cell[i][j].g + cost;
-        let hNew = calculateH(ni, nj, dest);
-        let fNew = gNew + hNew;
-
-        if (cell[ni][nj].f > fNew || isDestination(ni, nj, dest)) {
-          cell[ni][nj] = {
-            f: fNew,
-            g: gNew,
-            h: hNew,
-            parent_i: i,
-            parent_j: j
-          };
-          if (isDestination(ni, nj, dest)) {
-            return tracePath(cell, dest, exploredCount, exploredNodes);
-          }
-          newOpen.push({ f: fNew, i: ni, j: nj });
-        }
-      }
-    }
-
-    newOpen.sort((a, b) => a.f - b.f);
-
-    open = newOpen.slice(0, k);
-  }
-
-  return { path: [], pathCost: 0, exploredCount: exploredCount, exploredNodes: exploredNodes };
-}
-
-
-export {beamSearch, aStarSearch}
+export { aStarSearch };
